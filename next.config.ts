09e047ec493149a3cff36f7f18a51e7ac723beb6
Jordan Maslyn <jordan.maslyn@webflow.com>
2025-10-03 09:30:07 -0400

    Allow base path config via env vars

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   poweredByHeader: false,
-  basePath: '/config',
+  basePath: process.env.BASE_PATH === 'root' ? undefined : process.env.BASE_PATH ?? '/config',
 };
 
 export default nextConfig;
